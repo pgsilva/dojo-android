4f642053fd7ce7a2bde249e5683843de46246d2e
Paulo Guilherme <pgsilva0698@gmail.com>
2023-12-29 22:17:02 -0300

    feat(views and layout):🎨 add first version layout to search page

--- app/src/main/java/com/example/dojo/MainActivity.kt
+++ app/src/main/java/com/example/dojo/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.dojo
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Hello Mr Morales", Toast.LENGTH_LONG).show()
